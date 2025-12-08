eba97f029d85b928bb64f452cca67a44891f30e2
Hasset Admasu <188715591+Hasset-hub@users.noreply.github.com>
2025-12-08 06:13:27 +0000

    Fixed the linting by adding imports

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -9,6 +9,7 @@ from flask import Flask, Response, jsonify, request
 from presidio_anonymizer import AnonymizerEngine, DeanonymizeEngine
 from presidio_anonymizer.entities import InvalidParamError
 from presidio_anonymizer.services.app_entities_convertor import AppEntitiesConvertor
+from presidio_anonymizer.services.custom_operators import GenZOperator
 from werkzeug.exceptions import BadRequest, HTTPException
 
 DEFAULT_PORT = "3000"
